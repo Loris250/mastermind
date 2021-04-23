0c6e793bc4ca276d8da109bfd3a84ff39b7143ab
Loris <l@example.com>
2021-04-23 21:40:35 +0200

    axel

--- src/main.py
+++ src/main.py
@@ -2,36 +2,38 @@ import random
 
 ### variables ###
 
-colors = ["blue", "green", "yellow", "red", "orange", "grey"] # colors to be used in game
+colors: list = ["blue", "green", "yellow", "red", "orange", "grey"] # colors to be used in game
 
-code = [] # list containing random arrangement of 4 colors to be found out be user
+code: list = [] # list containing random arrangement of 4 colors to be found out be user
 
-response = [] # list containing black or white as response to user_list
+response: list = [] # list containing black or white as response to user_list
 
-gameWon = False # boolean to know if the user has found the code
+gameWon: bool = False # boolean to know if the user has found the code
 
 ### functions ###
 
 def getList():
-    input_string = input("Enter a code of four colors (blue, green, yellow, red, orange or grey) separated by a space: ")
-    user_list = input_string.split()
+    input_string: str = input("Enter a code of four colors (blue, green, yellow, red, orange or grey) separated by a space: ")
+    user_list: list = input_string.split()
     return user_list
 
 ### code ###
 
-for i in range(4):                          # generating random code
-    code = code + [random.choice(colors)]
+
+for i in range(4): # generating random code
+    code.append(random.choice(colors))
+
 
 code_copy = code
 print(code)
 
+user_list = []
 
-user_list = getList()       # ask user to enter a code
-print(user_list)
-
-if len(code) != len(user_list):             # verification for uncapable users :)
-    print("Enter a code of length 4")
-    user_list = getList()
+while len(code) != len(user_list):         
+    user_list = getList()                   # ask user to enter a code
+    print(user_list)
+    if len(code) != len(user_list):
+        print("Enter a code of length 4 !!!!")   # verification for uncapable users :)
 
 
 while gameWon == False:
@@ -42,6 +44,7 @@ while gameWon == False:
         response = 4*["black"]
         print("Congrats,you have found the code!")
         gameWon = True
+        break
 
     # compare exact positions
     for i in range(4):
@@ -51,7 +54,6 @@ while gameWon == False:
 
 
     # compare colors
-
     for i in range(4):
          for n in range(4):
              if user_list[i]== code_copy[n]:
