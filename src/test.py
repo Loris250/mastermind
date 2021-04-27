e071bf7f2b46d9a936f5257d5ac50ab6dab85129
Loris <l@example.com>
2021-04-27 16:47:00 +0200

    corrections

---
+++ src/test.py
@@ -0,0 +1,4 @@
+liste = [1,2,3,4]
+print(len(liste))
+liste2 = ["e", "u", "f", "fuck"]
+print(len(liste2))
--- src/version2.py
+++ src/version2.py
@@ -33,6 +33,7 @@ def generercode():
 
 code = generercode() # on commence par générer un code: une séquence de 4 couleurs 
 
+print(len(code))
 
 
 
@@ -41,23 +42,28 @@ code = generercode() # on commence par générer un code: une séquence de 4 cou
 while len(code) != len(user_list):         
     user_list = getList()                   # ask user to enter a code
     print("La liste de couleur entrée par l'utilisateur est: ", user_list)
+    print(len(user_list))
     a = 1
     for i in range(4): 
         if user_list[i] not in colors: 
             print("Entrez une liste avec les couleurs qui sont données")
             a = 2
             break 
+
+    if not len(code) == len(user_list):
+        print("Enter a code of length 4 !!!!") 
+        verification = 0    
+        a = 2
+
     if a == 1: 
         print("Le format de la liste entrée par l'utilisateur est validé ")
         verification = 1 # on peut maintenant commencer à comparer les listes
 
-    else: 
-        print("Le format de la liste entrée par l'utilisateur n'est pas validé")
+
+
+    if a == 2: 
+        print("Le format de la liste entrée par l'utilisateur n'est pas validé, veuillez entrer une nouvelle liste")
         user_list = []
-
-    if len(code) != len(user_list):
-        print("Enter a code of length 4 !!!!")   # verification for uncapable users :)
-
     #######
 
 
@@ -77,6 +83,8 @@ while gameWon == False and verification ==  1:
         if code_copy[i] == user_list[i]:
             response.append("black")
             code_copy[i] = "0"
+            user_list[i] = "rien"
+
     print(code_copy)
     print(response)
 
@@ -91,6 +99,5 @@ while gameWon == False and verification ==  1:
                 code_copy[n] = "0"
     print(code_copy)
     print(response)
-    gameWon = True
 
-                 
+    break
